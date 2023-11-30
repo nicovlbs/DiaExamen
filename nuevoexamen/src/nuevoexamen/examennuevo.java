9143f58d157843093dee14f1dbbf1c13a7f186a0
Nico <Nico@192.168.1.101>
2023-11-30 11:45:08 +0100

    EXAMEN MODIFICADO

---
+++ nuevoexamen/src/nuevoexamen/examennuevo.java
@@ -0,0 +1,12 @@
+package nuevoexamen;
+
+public class examennuevo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hoy aprobaré el examen");
+
+	}
+
+}
